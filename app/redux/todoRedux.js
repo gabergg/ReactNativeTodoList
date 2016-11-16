b806ebb744f1e9fc6dce344abc860c7c0668bca6
Gabe G'Sell <gabergg@gmail.com>
2016-11-16 15:29:02 -0800

    naming

--- app/redux/todoRedux.js
+++ app/redux/todoRedux.js
@@ -9,11 +9,11 @@ export const actionCreators = {
   addItem: (item) => {
     return {type: types.ADD_ITEM, payload: item}
   },
-  removeItem: (item) => {
-    return {type: types.REMOVE_ITEM, payload: item}
+  removeItem: (index) => {
+    return {type: types.REMOVE_ITEM, payload: index}
   },
-  toggleItemCompleted: (item) => {
-    return {type: types.TOGGLE_ITEM_COMPLETED, payload: item}
+  toggleItemCompleted: (index) => {
+    return {type: types.TOGGLE_ITEM_COMPLETED, payload: index}
   },
   removeCompleted: (item) => {
     return {type: types.REMOVE_COMPLETED, payload: item}
