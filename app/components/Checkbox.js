d7de2325d25c33d3b02a3b7cc4603130385db485
Davis Whitehead <dwhitehead@eq-0366.lan>
2017-12-17 16:43:16 -0500

    finished with app layout section

--- app/components/Checkbox.js
+++ app/components/Checkbox.js
@@ -1,36 +1,11 @@
 import React, { Component, PropTypes } from 'react'
-import { View, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const styles = StyleSheet.create({
-  box: {
-    height: 20,
-    width: 20,
-    borderWidth: 2,
-    borderColor: 'black',
-  },
-  inner: {
-    flex: 1,
-    margin: 2,
-    backgroundColor: 'rgba(0,0,0,0.8)',
-  },
 })
 
 export default class Checkbox extends Component {
-
-  static propTypes = {
-    onToggle: PropTypes.func,
-    isChecked: PropTypes.bool,
-  }
-
   render() {
-    const {onToggle, isChecked} = this.props
-
-    return (
-      <TouchableOpacity onPress={onToggle}>
-        <View style={styles.box}>
-          { isChecked && <View style={styles.inner}/> }
-        </View>
-      </TouchableOpacity>
-    )
+    return null
   }
 }
--- app/components/List.js
+++ app/components/List.js
@@ -1,75 +1,37 @@
 import React, { Component, PropTypes } from 'react'
-import {
-  View,
-  Text,
-  ScrollView,
-  StyleSheet,
-  TouchableOpacity
-} from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TouchableOpacity } from 'react-native'
 
 import Checkbox from './Checkbox'
 
 const styles = StyleSheet.create({
-  container: {
+  listContainer: {
     flex: 1,
-  },
-  item: {
-    padding: 15,
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-between',
     alignItems: 'center',
-    borderBottomWidth: 1,
-    borderBottomColor: 'whitesmoke',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
   },
-  rightSection: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  remove: {
-    marginLeft: 10,
-    marginBottom: 2,
-    color: '#CD5C5C',
-    fontSize: 26,
-  },
-  completed: {
-    backgroundColor: 'whitesmoke',
+  boxSmall: {
+    width: 50,
+    height: 50,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: 'orange',
   },
 })
 
 export default class List extends Component {
 
-  static propTypes = {
-    items: PropTypes.array.isRequired,
-    onRemoveItem: PropTypes.func.isRequired,
-    onToggleItemCompleted: PropTypes.func.isRequired,
-  }
-
-  renderItem = (item, i) => {
-    const {onToggleItemCompleted, onRemoveItem} = this.props
-    const itemStyle = item.completed ? [styles.item, styles.completed] : styles.item
-
-    return (
-      <View key={i} style={itemStyle}>
-        <Text> {item.label} </Text>
-        <View style={styles.rightSection}>
-          <Checkbox
-            isChecked={item.completed}
-            onToggle={() => onToggleItemCompleted(i)}
-          />
-          <TouchableOpacity onPress={() => onRemoveItem(i)}>
-            <Text style={styles.remove}> &times; </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    )
-  }
-
   render() {
-    const {items} = this.props
-
     return (
-      <ScrollView style={styles.container}>
-        {items.map(this.renderItem)}
+      <ScrollView contentContainerStyle={styles.listContainer}>
+        <View style={styles.boxSmall} />
+        <View style={styles.boxSmall} />
+        <View style={styles.boxSmall} />
+        <View style={styles.boxSmall} />
+        <View style={styles.boxSmall} />
       </ScrollView>
     )
   }
